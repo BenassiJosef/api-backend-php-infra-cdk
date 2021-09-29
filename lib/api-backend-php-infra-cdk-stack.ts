bd6234b92c6a8c6ea8d180c8be5c00c0cca981ae
BenassiJosef <benassijosef@gmail.com>
2021-09-29 18:25:38 +0100

    first pipeline

--- lib/api-backend-php-infra-cdk-stack.ts
+++ lib/api-backend-php-infra-cdk-stack.ts
@@ -1,9 +1,26 @@
 import * as cdk from '@aws-cdk/core';
+import { CodePipeline, CodePipelineSource, ShellStep } from "@aws-cdk/pipelines";
 
 export class ApiBackendPhpInfraCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      // The pipeline name
+      pipelineName: 'MyServicePipeline',
+
+       // How it will be built and synthesized
+       synth: new ShellStep('Synth', {
+         // Where the source can be found
+         input: CodePipelineSource.gitHub('BenassiJosef/api-backend-php-infra-cdk', 'main'),
+         
+         // Install dependencies, build and run cdk synth
+         commands: [
+           'npm ci',
+           'npm run build',
+           'npx cdk synth'
+         ],
+       }),
+    });
   }
 }
