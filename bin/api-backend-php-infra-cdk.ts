00c4dbec453296b08a0c0ba0ffa6cab62861c265
BenassiJosef <benassijosef@gmail.com>
2021-10-06 19:51:44 +0100

    changes to pipelines and ecs structure

--- bin/api-backend-php-infra-cdk.ts
+++ bin/api-backend-php-infra-cdk.ts
@@ -1,41 +1,57 @@
 #!/usr/bin/env node
+import { PipelineStack } from '../lib/pipeline-stack';
+import {EcsAppStack} from "../lib/ecs-app-stack"
 import * as cdk from '@aws-cdk/core';
-import * as ecs from "@aws-cdk/aws-ecs"
-import { ApplicationPipeline } from '../lib/application-pipeline';
-import {EcsFargateService} from "../lib/ecs-fargate-service"
-import { CodePipeline, CodePipelineSource, ShellStep } from '@aws-cdk/pipelines';
 
 const app = new cdk.App();
 
-interface AppStageProps extends cdk.StackProps {
-  serviceName:string,
-  clusterName: string
-}
-class AppStage extends cdk.Stage {
-  public readonly fI: any;
-  public readonly stageFargateInstance: any;
-  constructor(scope: cdk.Construct, id: string, props: AppStageProps ) {
-    super(scope, id, props);
+const ecsStackName = "EcsStackDeployedInPipeline"
 
-    const stageFargateInstance = new EcsFargateService(this, props.serviceName,{
-      clusterName:props.clusterName
-    })
+// the CodePipeline Stack needs to be created first
+const pipelineStack = new PipelineStack(app, 'aws-cdk-pipeline-ecs-separate-sources',{
+  env: { account: '511089130325', region: 'eu-west-1' },
+  ecsStackName
+});
 
-    this.fI = {
-      cluster:stageFargateInstance.fargateInstance.service.cluster,
-      serviceArn:stageFargateInstance.fargateInstance.service.serviceArn,
-      serviceName: stageFargateInstance.fargateInstance.service.serviceName,
-      stack: stageFargateInstance.fargateInstance.service.stack,
-      env:stageFargateInstance.fargateInstance.service.env,
-      node:stageFargateInstance.fargateInstance.service.node
-    }
-    new ApplicationPipeline(app,"Application-Pipeline-Test",{
-        env: { account: '511089130325', region: 'eu-west-1' },
-        stageFargateService: stageFargateInstance.fargateInstance.service
-      })
-  }
-  
-}
+// we supply the image to the ECS application Stack from the CodePipeline Stack
+new EcsAppStack(app, ecsStackName, {
+  env:{account:'069793231881' ,region:'eu-west-1'},
+  image: pipelineStack.tagParameterContainerImage,
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// interface AppStageProps extends cdk.StackProps {
+//   serviceName:string,
+//   clusterName: string
+// }
+// class AppStage extends cdk.Stage {
+//   public readonly iBaseServiceInstance: any;
+
+//   constructor(scope: cdk.Construct, id: string, props: AppStageProps ) {
+//     super(scope, id, props);
+
+//     const fargateResources = new EcsFargateService(this, props.serviceName,{
+//       clusterName:props.clusterName
+//     })
+//     this.iBaseServiceInstance = {
+//       cluster : fargateResources.fargateServiceCluster,
+//       serviceArn: fargateResources.fargateServiceArn
+//     }
+//   }
+// }
 
 // interface ApplicationPipelineProps extends cdk.StackProps {
 //   stageFargateService: ecs.IBaseService
@@ -52,46 +68,46 @@ class AppStage extends cdk.Stage {
 //   }
 // }
 
-
-class CdkPipeline extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-    const phpCdkPipeline = new CodePipeline(this, 'PhpCdkInfraPipeline', {
-      // The pipeline name
-      pipelineName: 'MyServicePipeline',
-      crossAccountKeys:true,
+// class CdkPipeline extends cdk.Stack {
+//   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+//     super(scope, id, props);
+//     const phpCdkPipeline = new CodePipeline(this, 'PhpCdkInfraPipeline', {
+//       // The pipeline name
+//       pipelineName: 'MyServicePipeline',
+//       crossAccountKeys:true,
     
-       // How it will be built and synthesized
-       synth: new ShellStep('Synth', {
-         // Where the source can be found
-         input: CodePipelineSource.gitHub('BenassiJosef/api-backend-php-infra-cdk', 'main', {
-          authentication: cdk.SecretValue.secretsManager('github-token'),
-        }),
+//        // How it will be built and synthesized
+//        synth: new ShellStep('Synth', {
+//          // Where the source can be found
+//          input: CodePipelineSource.gitHub('BenassiJosef/api-backend-php-infra-cdk', 'main', {
+//           authentication: cdk.SecretValue.secretsManager('github-token'),
+//         }),
          
-         // Install dependencies, build and run cdk synth
-         commands: [
-           'npm ci',
-           'npm run build',
-           'npx cdk synth'
-         ],
-       }),
-    });
+//          // Install dependencies, build and run cdk synth
+//          commands: [
+//            'npm ci',
+//            'npm run build',
+//            'npx cdk synth'
+//          ],
+//        }),
+//     });
 
-    const infraFargateStage = new AppStage(app,'Deploy-AppStage-Stage-Fargate',{
-      env:{account:'069793231881' ,region:'eu-west-1'},
-      serviceName: "Stage-Fargate",
-      clusterName:"stage-cluster-stampedeExample"
-    })
-    phpCdkPipeline.addStage(infraFargateStage)
+//     const infraFargateStage = new AppStage(app,'Deploy-AppStage-Stage-Fargate',{
+//       env:{account:'069793231881' ,region:'eu-west-1'},
+//       serviceName: "Stage-Fargate",
+//       clusterName:"stage-cluster-stampedeExample"
+//     })
+//     phpCdkPipeline.addStage(infraFargateStage)
+   
+//     console.log("ibaseserviceinstance: ", infraFargateStage.iBaseServiceInstance)
+    
+//     phpCdkPipeline.addStage(new ApplicationPipelineStage(app,"Application-Pipeline-Test",{
+//       stageFargateService: infraFargateStage.iBaseServiceInstance
+//     }))
+//   }
+// } 
 
-    // phpCdkPipeline.addStage(new ApplicationPipelineStage(app,"Application-Pipeline-Test",{
-    //   stageFargateService: infraFargateStage.fI
-    // }))
-
-  }
-}
-
-new CdkPipeline(app,"CdkPipeline",{
-  env: { account: '511089130325', region: 'eu-west-1' },
-})
+// new CdkPipeline(app,"CdkPipeline",{
+//   env: { account: '511089130325', region: 'eu-west-1' },
+// })
 
